a5472c3f74b7e2d532e59ef02f6c5109273ce7c9
defalque <marcodefalco.work@gmail.com>
2026-04-21 17:56:29 +0200

    Enhance documentation and styling for Getting Started page
    - Added a new Getting Started page with installation instructions and usage examples for Better Toast.
    - Implemented syntax highlighting for code snippets using Highlight.js for better readability.
    - Improved CSS styles for the documentation layout, including a responsive design and dark mode support.
    - Updated navigation structure with a table of contents for easier access to sections within the documentation.

--- projects/website/src/app/components/headless-component/headless-component.ts
+++ projects/website/src/app/components/headless-component/headless-component.ts
@@ -6,33 +6,37 @@ import { ToasterService } from 'better-toast';
   imports: [],
   template: `
     <div
-      class="w-fit inset-shadow-md shadow-lg shadow-black/10 ring-1 ring-black/10  px-4 py-2 rounded-2xl bg-zinc-50/10 backdrop-blur-sm "
+      class="w-fit inset-shadow-md shadow-lg shadow-black/10 ring-1 ring-black/10 dark:ring-zinc-800/90 px-4 py-2 rounded-2xl bg-zinc-50/10 backdrop-blur-sm dark:bg-zinc-950/10"
     >
       <div class="grid gap-2">
-        <p class="text-[0.65rem] uppercase font-semibold text-zinc-500 ">Currently playing</p>
+        <p class="text-[0.65rem] uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+          Currently playing
+        </p>
 
         <div class="flex gap-2">
           <div class="size-14 rounded-lg overflow-hidden">
             <img [src]="songImage()" alt="" class="size-full object-cover" />
           </div>
           <div class="self-center">
-            <h3 class="text-lg font-semibold tracking-tight text-zinc-800">
+            <h3 class="text-lg font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
               {{ songTitle() }}
             </h3>
-            <p class="text-[15px] text-zinc-700">{{ songArtist() }}</p>
+            <p class="text-[15px] text-zinc-700 dark:text-zinc-300">{{ songArtist() }}</p>
           </div>
         </div>
 
         <div class="flex items-center gap-3 w-full min-w-[220px] pt-0.5 px-1">
-          <div class="text-[11px] tabular-nums text-zinc-500 shrink-0 font-medium">
-            {{ formatTime(63) }}
+          <span
+            class="text-[12px] tabular-nums text-zinc-500 dark:text-zinc-600 shrink-0 font-medium"
+            >{{ formatTime(63) }}</span
+          >
+          <div class="flex-1 min-w-0 w-62 h-1.5 rounded-full cursor-pointer bg-gray-400/50 mt-1">
+            <div class="h-full rounded-full dark:bg-zinc-100 bg-zinc-800 w-2/5"></div>
           </div>
-          <div class="flex-1 min-w-0 w-62 h-1.5 rounded-full cursor-pointer bg-gray-400/50 mb-0">
-            <div class="h-full rounded-full bg-zinc-800 w-2/5"></div>
-          </div>
-          <div class="text-[11px] tabular-nums text-zinc-500 shrink-0 font-medium">
-            {{ formatTime(157) }}
-          </div>
+          <span
+            class="text-[12px] tabular-nums text-zinc-500 dark:text-zinc-600 shrink-0 font-medium"
+            >{{ formatTime(157) }}</span
+          >
         </div>
 
         <div class="flex items-center gap-2">
@@ -46,7 +50,7 @@ import { ToasterService } from 'better-toast';
               stroke-width="2"
               stroke-linejoin="round"
               stroke-linecap="round"
-              class="text-zinc-400 size-6"
+              class="text-zinc-400 dark:text-zinc-400 size-6"
             >
               <path
                 d="M12 2l2.9 6.26L22 9.27l-5 4.87L18.18 22 12 18.56 5.82 22 7 14.14 2 9.27l7.1-1.01L12 2z"
@@ -65,7 +69,7 @@ import { ToasterService } from 'better-toast';
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="size-13.5 text-zinc-800"
+                class="size-13.5 text-zinc-800 dark:text-zinc-100"
               >
                 <path d="M11 7.8l-5 4.2 5 4.2c.4.35 1 .05 1-.5V8.3c0-.55-.6-.85-1-.5z" />
                 <path d="M17.2 7.8l-5 4.2 5 4.2c.4.35 1 .05 1-.5V8.3c0-.55-.6-.85-1-.5z" />
@@ -82,7 +86,7 @@ import { ToasterService } from 'better-toast';
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="size-12.5 text-zinc-800"
+                class="size-12.5 text-zinc-800 dark:text-zinc-100"
               >
                 <rect x="7" y="5" width="4.5" height="14" rx="1.5" />
                 <rect x="12.5" y="5" width="4.5" height="14" rx="1.5" />
@@ -99,7 +103,7 @@ import { ToasterService } from 'better-toast';
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="rotate-180 size-13.5 text-zinc-800"
+                class="rotate-180 size-13.5 text-zinc-800 dark:text-zinc-100"
               >
                 <path d="M11 7.8l-5 4.2 5 4.2c.4.35 1 .05 1-.5V8.3c0-.55-.6-.85-1-.5z" />
                 <path d="M17.2 7.8l-5 4.2 5 4.2c.4.35 1 .05 1-.5V8.3c0-.55-.6-.85-1-.5z" />
@@ -117,7 +121,7 @@ import { ToasterService } from 'better-toast';
               stroke-width="2"
               stroke-linejoin="round"
               stroke-linecap="round"
-              class="text-zinc-400 size-6"
+              class="text-zinc-400 dark:text-zinc-400 size-6"
             >
               <path
                 d="M12 2l2.9 6.26L22 9.27l-5 4.87L18.18 22 12 18.56 5.82 22 7 14.14 2 9.27l7.1-1.01L12 2z"
--- projects/website/src/app/pages/docs/doc-getting-started/doc-getting-started.ts
+++ projects/website/src/app/pages/docs/doc-getting-started/doc-getting-started.ts
@@ -1,7 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import hljs from 'highlight.js';
+import bash from 'highlight.js/lib/languages/bash';
+import typescript from 'highlight.js/lib/languages/typescript';
+
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('typescript', typescript);
 
 @Component({
   selector: 'app-doc-getting-started',
   templateUrl: './doc-getting-started.html',
+  host: {
+    class: 'block w-full min-w-0 max-w-5xl mx-auto',
+  },
 })
-export class DocGettingStarted {}
+export class DocGettingStarted {
+  protected readonly toastInstallationSource = computed(() => {
+    return hljs.highlight(`npm install better-toast`, { language: 'bash' }).value;
+  });
+
+  protected readonly toastUsageSource = computed(() => {
+    return hljs.highlight(
+      `import { BetterToaster, ToasterService } from 'better-toast';
+import { inject, ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [BetterToaster],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: \`
+    <better-toaster />
+    <router-outlet />
+  \`,
+})
+export class App {}`,
+      { language: 'typescript' },
+    ).value;
+  });
+
+  protected readonly toastTypesSource = computed(() => {
+    return hljs.highlight(
+      `import { BetterToaster, ToasterService } from 'better-toast';
+import { inject, ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [BetterToaster],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: \`
+    <better-toaster />
+    <button (click)="showToast()">Show Toast</button>
+    <router-outlet />
+  \`,
+}
+export class App {
+  protected readonly toaster = inject(ToasterService);
+
+  protected showToast(): void {
+      this.toaster.show('Hello, world!');
+  }
+}`,
+      { language: 'typescript' },
+    ).value;
+  });
+}
